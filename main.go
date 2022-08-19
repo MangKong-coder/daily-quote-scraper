2be5e458672a15a09b0dcfb4cb25e06efc7c4c8c
MangKong-coder <domango1214@gmail.com>
2022-08-19 21:44:46 +0000

    feat: change time

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	for {
 		currentTime := time.Now().Local().Format("15:04:05")
 		// sends email at 8AM in the morning
-		if currentTime == "12:00:00" {
+		if currentTime == "10:00:00" {
 			images := scraper.ScrapeQuoteImages()
 			var htmlString string
 			for _, j := range images {
