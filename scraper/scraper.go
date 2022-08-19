9942567a6e364f3215a7d659d10ab4511be4a9dd
MangKong-coder <domango1214@gmail.com>
2022-08-19 04:26:09 +0000

    feat: create `scraper.go`

---
+++ scraper/scraper.go
@@ -0,0 +1,33 @@
+package scraper
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ScrapeQuoteImages() []string {
+	var images []string;
+	res,err := http.Get("https://www.insightoftheday.com/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	  }
+	
+	  // Load the HTML document
+	  doc, err := goquery.NewDocumentFromReader(res.Body)
+	  if err != nil {
+		log.Fatal(err)
+	  }
+	  // Find the quote images
+	  doc.Find(".img-responsive").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		band, _ := s.Attr("src")
+		images = append(images, band)
+	  })
+	  return images
+}
